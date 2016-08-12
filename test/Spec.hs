289b6d84812b8c1acb441b1a01a60721d6ac9f4f
Dustin Rohde <dustin.rohde@gmail.com>
2016-08-11 21:45:37 -0400

    Add test for serializing Requests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,18 @@
+import Control.Monad (void)
 import Data.Aeson (encode, decode)
 import qualified Data.ByteString.Char8 as S8
 import Test.HUnit
+import Network.HTTP.Client (Request, defaultRequest)
 
 import Restless
 
-testRequestEncodeDecode :: Assertion
+testRequestEncodeDecode = TestCase $ do
+    let x = encode defaultRequest
+        y = decode x :: Maybe Request
+        z = encode y
+    assertEqual "Response should be serializable to and from JSON" x z
+
+tests = TestList [testRequestEncodeDecode]
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = void $ runTestTT tests
